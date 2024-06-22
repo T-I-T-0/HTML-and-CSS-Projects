120c674778c4edb888074c490cf2ecc87db47d8d
T-I-T-0 <mirzaei.mani@outlook.com>
2024-06-21 18:24:53 -0700

    These are for html and js

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+alert("Hello, World! I am an alert box!");
+
+
+<script>
+    function My_First_Function() {
+        
+        var string ="Kiss me, I'm Irish!";
+        var result = string.fontcolor("green");
+        document.getElementById("Irish").innerHTML =
+        result ;
+
+
+    }
+</script>
